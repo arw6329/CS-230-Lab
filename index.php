cc0904e0b15a040c8196e9ddafc3beb76e67c97a
arw6329 <arw0048@mix.wvu.edu>
2021-02-03 14:39:58 -0500

    Lab 2 - index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<main>
+	<title>About us</title>
+    <link rel="stylesheet" href="css/about.css">
+    <div class="about">
+        <h1>Our Team</h1>
+        <p>Meet the amazing people who make us possible</p>
+    </div>
+    <div class="row">
+		<div class="card">
+			<img src="images/austin-williams.jpg">
+			<div class="container">
+				<h2><span class="name">Austin Williams</span> - CEO</h2>
+				<hr>
+				<p>Owner of the company. Born in Wheeling WV.</p>
+				<p>Email: dontemailme@gmail.com</p>
+			</div>
+		</div>
+		<div class="card">
+			<img src="images/paul-hollywood.jpg">
+			<div class="container">
+				<h2><span class="name">John Utt</span> - Web Design</h2>
+				<hr>
+				<p>Master of HTML with 2 decades experience in web design. Can be suspicious at times.</p>
+				<p>Email: john@gmail.com</p>
+			</div>
+		</div>
+		<div class="card">
+			<img src="images/michael-utt.jpeg">
+			<div class="container">
+				<h2><span class="name">Michael Utt</span> - Marketing</h2>
+				<hr>
+				<p>Unpredictable, but entertaining from a distance. Great at specific tasks.</p>
+				<p>Email: michael@gmail.com</p>
+			</div>
+		</div>
+		<div class="card">
+			<img src="images/bobby-flay.jpg">
+			<div class="container">
+				<h2><span class="name">Mark Utt</span> - Customer Support</h2>
+				<hr>
+				<p>A very satisfying person to talk to.</p>
+				<p>Email: mark@gmail.com</p>
+			</div>
+		</div>
+    </div>
+	<div class="company-info-block">
+		<img class="company-logo" src="images/popcorn.png">
+		<div class="company-info">
+			<h1>Our company</h1>
+			<p>We sell custom popcorn bowls. Our thoughtfully designed bowls bring a dimension of style and creativity to your favorite salted buttery snack.</p>
+			<p>Our mission is to make your next popcorn-related experience one you will never forget.</p>
+			<a href="javascript:document.querySelector('a').innerHTML='this will go to the shopping page when it exists';void 0">Start shopping</a>
+		</div>
+	</div>
+</main>
