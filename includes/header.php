b822b1c930076724712f02307c03aaf8efea9322
arw6329 <arw0048@mix.wvu.edu>
2021-03-09 23:37:32 -0500

    Lab 6

--- includes/header.php
+++ includes/header.php
@@ -43,6 +43,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="profile.php">Profile</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="admin.php">Admin</a>
+                    </li>
                     ';
                 } else {
                     echo '
